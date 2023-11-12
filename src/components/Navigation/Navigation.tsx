452d5118240eb680b197f313638a24693a030813
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-11-12 14:16:44 +0100

    Navbar with mobile sheet

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,10 +1,11 @@
-import { ArrowRight } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import { ArrowRight, Menu } from "lucide-react"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
+import { Link } from "react-router-dom"
 import { ModeToggle } from "../ModeToggle/ModeToggle"
 import { Button } from "../ui/button"
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
-import { Link } from "react-router-dom"
 
 export const Navigation = () => {
   const plLang = "https://cdn.kcak11.com/CountryFlags/countries/pl.svg"
@@ -21,11 +22,10 @@ export const Navigation = () => {
   }
 
   return (
-    <div className="w-screen py-2 px-6 border-b-2 flex justify-between items-center">
-      <div className="flex items-center space-x-8 ">
-        <div className="text-2xl font-semibold text-primary tracking-widest italic">
-          <h3>GmbH</h3>
-        </div>
+    <div className="w-screen py-2 px-4 border-b-2 flex justify-between items-center">
+      <h3 className="text-2xl font-semibold text-primary tracking-widest lg:hidden">GmbH</h3>
+      <div className="hidden lg:flex items-center space-x-8">
+        <h3 className="text-2xl font-semibold text-primary tracking-widest">GmbH</h3>
         <div className="space-x-2">
           <Button variant={"ghost"}>About</Button>
           <Button variant={"ghost"}>Current News</Button>
@@ -33,7 +33,7 @@ export const Navigation = () => {
         </div>
       </div>
 
-      <div className="flex items-center space-x-4">
+      <div className="hidden lg:flex items-center space-x-4">
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger>
@@ -44,7 +44,7 @@ export const Navigation = () => {
               >
                 <img
                   src={language === "en" ? plLang : engLang}
-                  className="w-4 h-4"
+                  className="w-6 h-6"
                 />
               </Button>
             </TooltipTrigger>
@@ -62,6 +62,61 @@ export const Navigation = () => {
             </div>
           </Link>
         </Button>
+      </div>
+      <div className="lg:hidden">
+        <Sheet>
+          <Button
+            asChild
+            size="icon"
+            variant="outline"
+          >
+            <SheetTrigger>
+              <Menu />
+            </SheetTrigger>
+          </Button>
+          <SheetContent>
+            <div className="h-full flex flex-col justify-between">
+              <div className="flex flex-col my-10 space-y-4">
+                <Button variant="ghost">About</Button>
+                <Button variant="ghost">Current News</Button>
+                <Button variant="ghost">Our Projects</Button>
+              </div>
+              <div className="flex flex-col justify-center space-y-4">
+                <Button asChild>
+                  <Link to={"/collaboration"}>
+                    <div className="space-x-2 flex items-center ">
+                      <span>Collaboration</span>
+                      <ArrowRight />
+                    </div>
+                  </Link>
+                </Button>
+                <div className="flex items-center justify-center space-x-2">
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <Button
+                          onClick={handleChangeLanguage}
+                          variant="secondary"
+                          size="icon"
+                        >
+                          <img
+                            src={language === "en" ? plLang : engLang}
+                            className="w-6 h-6"
+                          />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Switch to english</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+
+                  <ModeToggle />
+                </div>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   )
