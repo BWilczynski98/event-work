1e7dbeca81ce6c698c2c53e57e7b753125240a14
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-11-14 16:32:21 +0100

    Connect react context store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Navigation } from "./components/Navigation/Navigation"
 import { ThemeProvider } from "./components/ThemeProvider/ThemeProvider"
+import { ContextStore } from "./context/navigationRefsContext"
 import { HomePage } from "./pages"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="overflow-x-hidden">
-      <ThemeProvider>
-        <Navigation />
-        {children}
-      </ThemeProvider>
+      <ContextStore>
+        <ThemeProvider>
+          <Navigation />
+          {children}
+        </ThemeProvider>
+      </ContextStore>
     </div>
   )
 }
