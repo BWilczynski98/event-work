9ab9b920737f27a4c5e603739299e3067fa01667
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-11-14 16:39:12 +0100

    Added new components to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,17 @@
-import { Hero } from "@/components/Hero"
+import { Hero } from "@/pages/HomePage/components/Hero"
+import { About } from "./components/About"
+import { CurrentNews } from "./components/CurrentNews"
+import { OurProjects } from "./components/OurProjects"
+import { Footer } from "./components/Footer"
 
 export const HomePage = () => {
   return (
     <div>
       <Hero />
+      <About />
+      <CurrentNews />
+      <OurProjects />
+      <Footer />
     </div>
   )
 }
